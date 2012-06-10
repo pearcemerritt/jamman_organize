e4383a3e01afbbef9a48f5eced98f014cbcb3c45
Pearce Merritt <pearcemerritt@gmail.com>
2012-06-10 01:27:19 -0700

    added a opening block comment

--- jamman_organize.rb
+++ jamman_organize.rb
@@ -1,3 +1,23 @@
+####################################################################
+# File: jamman_organize.rb
+# Author: Pearce Merritt
+# Date: Saturday 10 June 2012
+#
+# This tool can be used to recurse through a JAMMAN folder
+# downloaded from the DigiTech JAMMAN and organize all the poorly
+# named loop files so that they resemble the folders that they
+# are kept in. Specifically:
+#    JAMMAN/LOOPNN/LOOP.WAV <-- JAMMAN/LOOPNN/LOOPNN.WAV
+#    JAMMAN/LOOPNN/LOOP.XML <-- JAMMAN/LOOPNN/LOOPNN.XML
+# The file directory structure is intact, though that may be
+# a possible next direction for this project in learning ruby.
+#
+# Note, this is my first real ruby program! Also, my first real
+# experience with the power of scripting languages on a local
+# machine. I've done a bit of Perl for school and a bit of 
+# JavaScript for school as well, but only for web development.
+####################################################################
+
 #! /usr/bin/env ruby
 
 Dir.chdir("JAMMAN")
@@ -7,6 +27,7 @@ Dir.chdir("JAMMAN")
 # LOOPNN, where NN is a number (zero padded) between 0 and 99.
 Dir.foreach(".") do |jfile|
 
+  # Ignore the trivial hidden directories and system files
   case
   when File.basename(jfile).eql?(".")
     next
@@ -15,7 +36,6 @@ Dir.foreach(".") do |jfile|
   when File.basename(jfile).eql?("SETUP.XML")
     next
   else
-    #puts "Directory: " + File.expand_path(jfile)
     puts File.basename(jfile) + ":"
 
     Dir.chdir(jfile) 
@@ -27,19 +47,10 @@ Dir.foreach(".") do |jfile|
       # Ignore the trivial hidden directories
       next if File.directory?(lfile)
 
-      #case
-      #when File.basename(lfile).eql?(".")
-        #next
-      #when File.basename(lfile).eql?("..")
-        #next
-      #else
-
       puts "  Changing " + lfile + " to " + jfile + File.extname(lfile)
 
       # LOOP.EXT <- LOOPNN.EXT
       File.rename(lfile, jfile + File.extname(lfile))
-
-      #end # LOOPNN files case
 
     end # LOOPNN files loop
 
